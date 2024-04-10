f04987f8f63719ab8c180ba28db0b782db3c6cc5
Sargis221 <ghazaryansargis005@gmail.com>
2024-04-10 08:08:31 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
@@ -19,26 +24,39 @@ const App = () => {
                             /* Add Budget component here */
                         }        
 
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         {
                             /* Add Remaining component here*/
                         }        
-
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {
                             /* Add ExpenseTotal component here */
                         }        
-                       
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         {
                             /* Add ExpenseList component here */
                         }         
-
+<div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {
                             /* Add ExpenseItem component here */
                         }        
-
+<div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {
                             /* Add AllocationForm component here under */
                         }        
-
+<div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
